6154ffda954ee248a2da9e940b195bb2990ada63
AdeleLip <adele.lip@gmail.com>
2022-05-09 14:56:42 +0100

    fix settings

--- allostery/__init__.py
+++ allostery/__init__.py
@@ -6,7 +6,7 @@ with open(f'{os.environ["ALLOSTERY_HOME"]}/config', 'r') as file:
     allostery_settings = yaml.load(file, Loader=yaml.FullLoader)
 
 # get current project settings
-__config_file = 'allostery_settings["project_location"]}/{allostery_settings["project"]}/.defaults/config'
+__config_file = f'{allostery_settings["location"]}/{allostery_settings["project"]}/.defaults/config'
 if not os.path.exists(__config_file):
     __config_file = f'{os.environ["ALLOSTERY_HOME"]}/data/project_default'  # change to default if no project settings
 
