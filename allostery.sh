43e3feeb7c05f544654a54cecf02849a371408f4
AdeleLip <adele.lip@gmail.com>
2022-05-09 11:30:58 +0100

    change pythonpath

--- allostery.sh
+++ allostery.sh
@@ -1,3 +1,9 @@
 export ALLOSTERY_HOME=/home/adele/Documents/allostery
 export PATH="$ALLOSTERY_HOME/bin:$PATH"
-export PYTHONPATH="$ALLOSTERY_HOME:$PYTHONPATH"
+
+if [ -z "$PYTHONPATH" ]; then
+  export PYTHONPATH="$ALLOSTERY_HOME"
+else
+  export PYTHONPATH="$ALLOSTERY_HOME:$PYTHONPATH"
+fi
+
