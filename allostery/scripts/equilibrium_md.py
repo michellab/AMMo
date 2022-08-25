fe26bf7cc4e03e37dd5fbe7e06d60220d50ba3f2
AdeleLip <adele.lip@gmail.com>
2022-08-25 13:47:17 +0100

    equilibrium MD reporting and workdir specification

--- allostery/scripts/equilibrium_md.py
+++ allostery/scripts/equilibrium_md.py
@@ -8,9 +8,12 @@ def __main__():
     parser.add_argument('--topology', type=str, help='parameter file')
     parser.add_argument('--coordinates', type=str, help='cooridnate file')
     parser.add_argument('--output', type=str, help='output location, without extension (e.g. "production-1"). If None, the next available name will be used (e.g. "production-3" if "production-1" and "production-2" already exist)')
+    parser.add_argument('--report', type=int, default=2500, help='Report interval in steps. Default: 2500')
+    parser.add_argument('--workdir', type=str, help='Working directory')
+    parser.add_argument('--clean', action='store_true', help='Remove unneeded process files')
     args = parser.parse_args()
 
-    run_eq_md(args.duration, args.topology, args.coordinates, args.output)
+    run_eq_md(args.duration, args.topology, args.coordinates, args.output, args.report, args.workdir, args.clean)
     return None
 
 
--- allostery/scripts/seeded_md.py
+++ allostery/scripts/seeded_md.py
@@ -3,7 +3,7 @@ from argparse import ArgumentParser
 from allostery.equilibrium import run_eq_md
 
 
-def run_seeded_md(folder, snapshot, duration):
+def run_seeded_md(folder, snapshot, duration, report=5000, workdir=None, clean=False):
     os.chdir(folder)
     if not os.path.exists(f'snapshot_{snapshot}'):
         os.mkdir(f'snapshot_{snapshot}')
@@ -15,17 +15,20 @@ def run_seeded_md(folder, snapshot, duration):
         topology = '../system-setup/system.prm7'
 
     #run seeded MD (i.e. equilibrium md using the seed input)
-    run_eq_md(duration, topology, f'snapshots/snapshot_{snapshot}.rst7', f'snapshot_{snapshot}/production')
+    run_eq_md(duration, topology, f'snapshots/snapshot_{snapshot}.rst7', f'snapshot_{snapshot}/production', report, os.path.abspath(workdir), clean)
     return None
 
 def __main__():
     parser = ArgumentParser(description='Run a seeded MD simulation')
     parser.add_argument('--folder', type=str, required=True, help='Seeded MD folder, containing topology and a snapshots directory')
-    parser.add_argument('--snapshot', type=str, required=True, help='seed index')
-    parser.add_argument('--duration', type=float, required=True, help='seeded MD duration in ns')
+    parser.add_argument('--snapshot', type=str, required=True, help='Seed index')
+    parser.add_argument('--duration', type=float, required=True, help='Seeded MD duration in ns')
+    parser.add_argument('--report', type=int, default=5000, help='Report interval in steps. Default: 2500')
+    parser.add_argument('--workdir', type=str, help='Working directory')
+    parser.add_argument('--clean', action='store_true', help='Remove unneeded process files')
     args = parser.parse_args()
 
-    run_seeded_md(args.folder, args.snapshot, args.duration)
+    run_seeded_md(args.folder, args.snapshot, args.duration, args.report, args.workdir, args.clean)
 
     return None
 
