d5b7f87d8f3f95bc3b8f8359e17a08569d76628d
AdeleLip <adele.lip@gmail.com>
2022-09-20 09:17:31 +0100

    move seed parsing to utils

--- allostery/utils/_utils.py
+++ allostery/utils/_utils.py
@@ -4,6 +4,7 @@ import os
 from random import choice
 from string import ascii_lowercase, digits
 import subprocess
+import numpy as np
 
 
 def get_dry_trajectory(topology, trajectory, output):
@@ -39,3 +40,24 @@ def get_dry_trajectory(topology, trajectory, output):
 
     return None
 
+def __parse_seeds(seeds):
+    if seeds == 'all':
+        seeds = [int(folder.split('_')[1]) for folder in os.listdir() if folder.startswith('snapshot_') and '.pdb' not in folder]
+        seeds.sort()
+    elif isinstance(seeds, (list, tuple)):
+        if not all(isinstance(idx, int) for idx in seeds):
+            raise TypeError('Seed indices must all be of type int')
+        else:
+            return seeds
+    elif isinstance(seeds, str):
+        if '-' in seeds:
+            idx_range = [int(idx) for idx in seeds.split('-')]
+            return np.arange(idx_range[0], idx_range[1]+1).tolist()
+        elif ',' in seeds:
+            return [int(idx) for idx in seeds.split(',')]
+        else:
+            return [int(seeds)]
+    elif isinstance(seeds, int):
+        return [seeds]
+    else:
+        raise TypeError('Seeds must be of type str, int, list or tuple')
